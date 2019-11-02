8e68a0a2e20e5855891c659996fdd0f87ce292de
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2019-11-02 16:29:39 +0100

    backend done but untested

--- src/backend/Controller.java
+++ src/backend/Controller.java
@@ -42,6 +42,59 @@ public class Controller {
         return result.toString();
     }
 
+    public void createAccount(String name, String address, String phoneNumber) throws Exception{
+        Class.forName("org.postgresql.Driver").newInstance();
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/anstdata?user=aj0533&password=5rsgoqk7");
+        PreparedStatement statement = con.prepareStatement("INSERT INTO Customer (name, address, email, phoneNumber) VALUES(?, ?, ?)");
+        statement.setString(1, name);
+        statement.setString(2, address);
+        statement.setString(3, phoneNumber);
+        ResultSet res = statement.executeQuery();
+
+        con.close();
+    }
+
+    public void adminShowUsers() throws Exception{
+        Class.forName("org.postgresql.Driver").newInstance();
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/anstdata?user=aj0533&password=5rsgoqk7");
+        PreparedStatement statement = con.prepareStatement("SELECT * FROM Customer");
+        ResultSet res = statement.executeQuery();
+        while(res.next()){
+            System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getString(3) + " " + res.getString(4));
+        }
+        con.close();
+    }
+
+    public boolean login(String email) throws Exception{
+        Class.forName("org.postgresql.Driver").newInstance();
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/anstdata?user=aj0533&password=5rsgoqk7");
+        PreparedStatement statement = con.prepareStatement("SELECT email FROM Customer WHERE email = " + email);
+        ResultSet res = statement.executeQuery();
+        con.close();
+        String result = res.getString(1);
+        return (result.length() > 0);
+    }
+
+    public boolean book(int travelId, int seats)throws Exception {
+        Class.forName("org.postgresql.Driver").newInstance();
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/anstdata?user=aj0533&password=5rsgoqk7");
+        PreparedStatement statement = con.prepareStatement("SELECT availableSeats FROM Travel WHERE travelId = " + travelId);
+        ResultSet res = statement.executeQuery();
+        int availableSeats = res.getInt(1);
+        if(seats <= availableSeats){
+            PreparedStatement updateStatement = con.prepareStatement("UPDATE Travel SET availableSeats = availableSeats - " + seats);
+            con.close();
+            return true;
+        }
+
+        con.close();
+        return false;
+    }
+
     public static void main(String[] args){
         Controller c = new Controller();
         try{
