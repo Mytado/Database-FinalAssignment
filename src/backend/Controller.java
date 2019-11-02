032e0805c369a094bf67a089cac6cbc1ea8e98b6
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2019-11-02 13:10:48 +0100

    started on backend

--- src/backend/Controller.java
+++ src/backend/Controller.java
@@ -1,4 +1,28 @@
 package backend;
 
+import java.sql.*;
+import java.util.*;
+
 public class Controller {
+
+    public String search(String to, String from) throws Exception{
+
+        Class.forName("org.postgresql.Driver").newInstance();
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/aj0533?user=aj0533&password=5rsgoqk7");
+        PreparedStatement statement = con.prepareStatement("SELECT * FROM Projekt");
+        ResultSet res = statement.executeQuery();
+        while(res.next()){
+            System.out.println(res.getInt(1) + " " + res.getString(2));
+        }
+        con.close();
+        String result = "";
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Controller c = new Controller();
+        c.search("", "");
+    }
 }
