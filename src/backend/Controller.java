b4d5606e2047825c291fafb1e530ffa25937d5d0
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2019-11-02 14:38:59 +0100

    ocd commit

--- src/backend/Controller.java
+++ src/backend/Controller.java
@@ -5,24 +5,50 @@ import java.util.*;
 
 public class Controller {
 
-    public String search(String to, String from) throws Exception{
+    public String search(String from, String to) throws Exception{
+        StringBuffer result = new StringBuffer();
+
+        String query = "SELECT travelId, travelFrom, travelTo, departure, arrival, price, seatsAvailable FROM Travel";
+        if(from.length() > 0 || to.length() > 0) {
+            query += " WHERE ";
+        }
+        if(from.length() > 0){
+            query += "travelFrom = " + from;
+        }
+        if(from.length() > 0 && to.length() > 0){
+            query += " AND ";
+        }
+        if(to.length() > 0){
+            query += "travelTo = " + to;
+        }
 
         Class.forName("org.postgresql.Driver").newInstance();
 
-        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/aj0533?user=aj0533&password=5rsgoqk7");
-        PreparedStatement statement = con.prepareStatement("SELECT * FROM Projekt");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/anstdata?user=aj0533&password=5rsgoqk7");
+        PreparedStatement statement = con.prepareStatement(query);
         ResultSet res = statement.executeQuery();
         while(res.next()){
-            System.out.println(res.getInt(1) + " " + res.getString(2));
+            result.append(res.getInt(1) + " "
+                    + res.getString(2) + " "
+                    + res.getString(3) + " "
+                    + res.getDate(4) + " "
+                    + res.getDate(5) + " "
+                    + res.getInt(6) + " "
+                    + res.getInt(7) + "\n");
         }
         con.close();
-        String result = "";
 
-        return result;
+
+        return result.toString();
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Controller c = new Controller();
-        c.search("", "");
+        try{
+            c.search("", "");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
