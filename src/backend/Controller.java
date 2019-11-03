f4c5c6bdba394bfbf3ef5fe8490bd99d5e1a1781
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2019-11-03 11:09:04 +0100

    fixed test

--- src/backend/Controller.java
+++ src/backend/Controller.java
@@ -101,7 +101,7 @@ public class Controller {
     public static void main(String[] args){
         Controller c = new Controller();
         try{
-            c.createAccount("Erik", "testgatan 1", "test@test.se", "782346238746");
+            c.createAccount("Erik", "testsson", "testgatan 1", 27614, "Malmoe", "test@test.se", "782346238746");
             c.adminShowUsers();
         }catch (Exception e){
             e.printStackTrace();
